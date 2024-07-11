f421fca1984e2d7241058a9ad7b3a86485aad46d
Rami Alkawadri <ramialkawadri@hotmail.com>
2024-07-11 22:44:36 +0200

    Hide lualine on file tree

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -1,3 +1,10 @@
 -- Documentation: https://github.com/nvim-lualine/lualine.nvim
 
-require("lualine").setup()
+require("lualine").setup {
+    options = {
+        disabled_filetypes = {
+            statusline = { "packer", "NvimTree" },
+            winbar = { "packer", "NvimTree" },
+        }
+    }
+}
