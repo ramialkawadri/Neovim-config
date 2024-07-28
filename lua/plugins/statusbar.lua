51a11eaadc5ca81b5bdabbf38e6269e67362d5c7
Rami Alkawadri <ramialkawadri@hotmail.com>
2024-07-28 19:07:26 +0200

    Update lualine

--- lua/plugins/statusbar.lua
+++ lua/plugins/statusbar.lua
@@ -10,6 +10,24 @@ return {
         },
         options = {
             section_separators = { left = '', right = '' },
-        }
+        },
+        sections = {
+            lualine_a = { { 'mode', separator = { left = '' }, padding = 1 } },
+            lualine_b = { 'filename', 'branch' },
+            lualine_c = { '%=', },
+            lualine_x = {},
+            lualine_y = { 'filetype', },
+            lualine_z = {
+                { 'location', separator = { right = '' }, padding = 1 },
+            },
+        },
+        inactive_sections = {
+            lualine_a = { 'filename' },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { 'location' },
+        },
     }
 }
