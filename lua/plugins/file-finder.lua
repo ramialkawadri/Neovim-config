e564800af0d2be53c0819c614030bf1bcd4119bb
ramiak <ramiak@netcompany.com>
2024-07-30 07:33:09 +0200

    Smart display

--- lua/plugins/file-finder.lua
+++ lua/plugins/file-finder.lua
@@ -4,6 +4,7 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         defaults = {
+            path_display = { "smart" },
             mappings = {
                 i = {
                     ["<C-w>"] = "delete_buffer"
@@ -14,6 +15,7 @@ return {
             }
         }
     },
+    -- TODO: move
     keys = {
         { "<F3>", [[:NvimTreeOpen<CR>]], desc = "Open File Tree" },
         { "<F4>", [[:NvimTreeClose<CR>]], desc = "Close File Tree" },
