ece4fa2bdeaa57ac88df7a33cbcd42f61514b962
Rami Alkawadri <ramialkawadri@hotmail.com>
2024-08-05 18:44:53 +0200

    Change line color

--- lua/config.lua
+++ lua/config.lua
@@ -48,5 +48,6 @@ vim.cmd.command("Format lua vim.lsp.buf.format()")
 
 vim.cmd.colorscheme("hybrid")
 vim.cmd.hi("MatchParen gui=underline guibg=None guifg=None guisp=white")
+vim.cmd.hi("LineNr guifg=#9ba6b1")
 -- Required for the LuaLine: https://github.com/neovim/neovim/pull/17266
 vim.opt.laststatus = 3
