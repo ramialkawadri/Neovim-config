0ae13af7f21a30959f21ffa9074cae03d9b33ace
Rami Alkawadri <ramialkawadri@hotmail.com>
2024-09-30 18:55:26 +0200

    Hide underline

--- lua/plugins/ibl-config.lua
+++ lua/plugins/ibl-config.lua
@@ -1,16 +1,21 @@
 return {
     "lukas-reineke/indent-blankline.nvim",
     config = function()
-    require("ibl").setup()
-    local highlight = {
-        "SelectedIndentationColor",
-    }
-    local hooks = require "ibl.hooks"
-    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
-        vim.api.nvim_set_hl(0, "SelectedIndentationColor", { fg = "#5d87ac" })
-    end)
+        local highlight = {
+            "SelectedIndentationColor",
+        }
+        local hooks = require "ibl.hooks"
+        hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+            vim.api.nvim_set_hl(0, "SelectedIndentationColor", { fg = "#5d87ac" })
+        end)
 
-    require("ibl").setup { scope = { highlight = highlight } }
-    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+        require("ibl").setup {
+            scope = {
+                highlight = highlight,
+                show_start = false,
+                show_end = false,
+            }
+        }
+        hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
     end
 }
