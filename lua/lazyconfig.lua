cf57a6128f41210485bd60eadaa19ae6ff063735
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-04-10 21:10:20 +0200

    Remove signs for todo comments

--- lua/lazyconfig.lua
+++ lua/lazyconfig.lua
@@ -51,7 +51,9 @@ require("lazy").setup({
         {
             "folke/todo-comments.nvim",
             dependencies = "nvim-lua/plenary.nvim",
-            opts = {},
+            opts = {
+                signs = false,
+            },
         },
         {
             "iamcco/markdown-preview.nvim",
