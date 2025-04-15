511c86f49faaf55a053ebce5c5a826033adedf80
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-04-15 10:07:51 +0200

    Move gitsigns to lua functions

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -3,11 +3,11 @@ return {
     lazy = false,
     opts = {},
     keys = {
-        { "gp", [[:Gitsigns preview_hunk<CR>]], desc = "Git Preview Hunk" },
-        { "gs", [[:Gitsigns stage_hunk<CR>]], desc = "Git Stage Hunk" },
-        { "gn", [[:Gitsigns next_hunk<CR>]], desc = "Git Next Hunk" },
-        { "gN", [[:Gitsigns prev_hunk<CR>]], desc = "Git Previous Hunk" },
-        { "gl", [[:Gitsigns blame_line<CR>]], desc = "Git Blame Line" },
-        { "grh", [[:Gitsigns reset_hunk<CR>]], desc = "Git Rest Hunk" },
+        { "gp",  function() require("gitsigns").preview_hunk() end,   desc = "Git Preview Hunk" },
+        { "gs",  function() require("gitsigns").stage_hunk() end,     desc = "Git Stage Hunk" },
+        { "gn",  function() require("gitsigns").nav_hunk('next') end, desc = "Git Next Hunk" },
+        { "gN",  function() require("gitsigns").nav_hunk('prev') end, desc = "Git Previous Hunk" },
+        { "gl",  function() require("gitsigns").blame_line() end,     desc = "Git Blame Line" },
+        { "grh", function() require("gitsigns").reset_hunk() end,     desc = "Git Rest Hunk" },
     },
 }
