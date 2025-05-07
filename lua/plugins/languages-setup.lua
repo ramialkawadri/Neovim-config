a6ff93a6b055ce007bc960e46c4cd6a664ab0013
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-05-07 21:10:37 +0200

    Make language setup not lazy

--- lua/plugins/languages-setup.lua
+++ lua/plugins/languages-setup.lua
@@ -1,6 +1,7 @@
 return {
     {
         "neovim/nvim-lspconfig",
+        lazy = false,
         config = function()
             local lspconfig = require("lspconfig")
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
