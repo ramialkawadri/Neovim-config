334c9420156329b3fdeb8cfc35db39d9195d38e0
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-08-28 16:26:51 +0200

    Use all recent files in telescope

--- lua/plugins/file-finder.lua
+++ lua/plugins/file-finder.lua
@@ -3,7 +3,10 @@ return {
     version = "*",
     dependencies = {
         "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-ui-select.nvim"
+        "nvim-telescope/telescope-ui-select.nvim",
+        "prochri/telescope-all-recent.nvim",
+        -- Required by the all recent extension
+        "kkharji/sqlite.lua",
     },
     config = function()
         require("telescope").setup {
@@ -16,7 +19,7 @@ return {
                 },
             },
             defaults = {
-                path_display = { "smart" },
+                path_display = { truncate = 8 },
                 mappings = {
                     i = {
                         ["<C-s>"] = "select_horizontal",
@@ -31,6 +34,25 @@ return {
         }
 
         require("telescope").load_extension("ui-select")
+
+        -- Load this after other extensions.
+        vim.g.sqlite_clib_path = vim.env.sqlite_clib_path
+        require("telescope-all-recent").setup({
+            vim_ui_select = {
+                kinds = {
+                    overseer_template = {
+                        use_cwd = true,
+                        prompt = "Task template",
+                        name_include_prompt = true,
+                    },
+                }, 
+                prompts = {
+                    ["Configuration: "] = {
+                        use_cwd = true,
+                    }
+                }
+            }
+        })
     end,
     lazy = false,
     keys = {
@@ -38,8 +60,8 @@ return {
         { "fg", function() require("telescope.builtin").live_grep() end,      desc = "RipGrep" },
         { "fb", function() require("telescope.builtin").buffers() end,        desc = "Search Buffers" },
         { "fh", function() require("telescope.builtin").help_tags() end,      desc = "Help Tags" },
-        { "fr", function() require("telescope.builtin").registers() end, desc = "Open Buffer Diagnostics" },
-        { "fm", function() require("telescope.builtin").marks() end, desc = "Open Buffer Diagnostics" },
+        { "fr", function() require("telescope.builtin").registers() end,      desc = "Open Buffer Diagnostics" },
+        { "fm", function() require("telescope.builtin").marks() end,          desc = "Open Buffer Diagnostics" },
         { "gr", function() require("telescope.builtin").lsp_references() end, desc = "Open Buffer Diagnostics" },
     },
 }
--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 1. [Ripgrep](https://github.com/BurntSushi/ripgrep/releases)
 1. `npm install --global vscode-langservers-extracted`
 1. `:MasonInstall roslyn`
+
+<!--TODO: install on the same directory for windows https://github.com/kkharji/sqlite.lua -->
