e4b41fa60818f25f9d6b95aeafb1d30a8fb796e6
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-10-26 08:17:25 +0100

    Add code to remember open and closed folds after closing a buffer

--- lua/plugins/auto-session-config.lua
+++ lua/plugins/auto-session-config.lua
@@ -1,12 +1,27 @@
+-- Remember which folds were used before leaving a buffer
+
+vim.api.nvim_create_autocmd({ "BufWinLeave" }, {
+    pattern = { "*.*" },
+    desc = "save view (folds), when closing file",
+    command = "mkview",
+})
+vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+    pattern = { "*.*" },
+    desc = "load view (folds), when opening file",
+    command = "silent! loadview"
+})
+
+-- Plugin configuration
+
 vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 return {
-  "rmagatti/auto-session",
-  lazy = false,
+    "rmagatti/auto-session",
+    lazy = false,
 
-  ---enables autocomplete for opts
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-  },
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+    },
 }
