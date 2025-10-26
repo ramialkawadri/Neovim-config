ee1084d051d6990e84af0d35e6e4f65f9422e3cc
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-10-26 06:58:30 +0100

    Add auto-session

---
+++ lua/plugins/auto-session-config.lua
@@ -0,0 +1,12 @@
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+
+return {
+  "rmagatti/auto-session",
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+  },
+}
