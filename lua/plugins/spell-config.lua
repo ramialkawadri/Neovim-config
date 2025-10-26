13e2a60bcac1493f8065907b00495f3f541dba8a
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-10-26 07:54:26 +0100

    Add shortcuts to add words to misspelled list

--- lua/plugins/spell-config.lua
+++ lua/plugins/spell-config.lua
@@ -4,8 +4,10 @@ return {
     "kamykn/spelunker.vim",
     lazy = false,
     keys = {
-        { "z=", function() vim.cmd [[call spelunker#correct_from_list()]] end, desc = "Spell suggestion" },
-        { "zn", function() vim.cmd [[call spelunker#jump_next()]] end,         desc = "Jump to next misspelled word" },
-        { "zN", function() vim.cmd [[call spelunker#jump_prev()]] end,         desc = "Jump to previous misspelled word" },
+        { "z=",  function() vim.cmd [[call spelunker#correct_from_list()]] end,                   desc = "Spell suggestion" },
+        { "zn",  function() vim.cmd [[call spelunker#jump_next()]] end,                           desc = "Jump to next misspelled word" },
+        { "zN",  function() vim.cmd [[call spelunker#jump_prev()]] end,                           desc = "Jump to previous misspelled word" },
+        { "zg",  function() vim.cmd [[call spelunker#execute_with_target_word("spellgood")]] end, desc = "Spell suggestion" },
+        { "zug", function() vim.cmd [[call spelunker#execute_with_target_word("spellundo")]] end, desc = "Spell suggestion" },
     },
 }
