2334003d4b69d6614cf4b8b88380da4145a31903
Rami Alkawadri <ramialkawadri@hotmail.com>
2025-11-10 21:47:58 +0100

    Compact grug and botright split

--- lua/plugins/grug-far.lua
+++ lua/plugins/grug-far.lua
@@ -1,10 +1,14 @@
 return {
     "MagicDuck/grug-far.nvim",
+    opts = {
+         showCompactInputs = true,
+         windowCreationCommand = "botright 80vsplit",
+    },
     keys = {
         {
             "<leader>f",
             mode = { "n", "v" },
-            function() require('grug-far').open() end,
+            function() require("grug-far").open() end,
             desc = "Open search"
         }
     }
